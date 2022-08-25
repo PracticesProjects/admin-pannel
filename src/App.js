71b8953b9dfbade578b7bdb7eaa1e7224996c340
Sagar1555 <stimalsena155@gmail.com>
2022-08-25 14:09:13 +0545

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Topbar from './components/topbar/Topbar'
-
+import Sidebar from './components/sidebar/Sidebar'
+import "./app.css"
 function App() {
   return (
     <div>
       <Topbar />
+    <div className="container">
+      <Sidebar />
+      <div className="others">Other Pages</div>
       
-
+    </div>
       </div>
   );
 }
