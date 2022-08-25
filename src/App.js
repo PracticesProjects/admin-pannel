0389be0e09135a1da061af55532747b32abda634
Sagar1555 <stimalsena155@gmail.com>
2022-08-25 14:37:51 +0545

    home page setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import './app.css'
 import Topbar from './components/topbar/Topbar'
 import Sidebar from './components/sidebar/Sidebar'
-import "./app.css"
+import Home from './pages/home/home'
 function App() {
   return (
     <div>
       <Topbar />
-    <div className="container">
-      <Sidebar />
-      <div className="others">Other Pages</div>
-      
-    </div>
+      <div className='container'>
+        <Sidebar />
+        <div className='others'>Other Pages</div>
+        <Home />
       </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
